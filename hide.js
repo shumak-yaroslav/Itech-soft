97e8a176a3e0c143f2f72c5de619ec6d533a38f5
yani.eniz <yani.eniz@yandex.by>
2020-07-28 15:24:29 +0300

    Fixed

--- hide.js
+++ hide.js
@@ -3,11 +3,16 @@ function personal () {
     let field1 = document.getElementById("field1");
     let check = document.getElementById("check");
     let btn1 = document.getElementById("btn1");
+    let second = document.getElementById("second");
 
     fields.style.display = "inline";
     field1.style.display = "inline";
+    field2.style.display = "none";
+    field3.style.display = "none";
     check.style.display = "none";
     btn1.style.display = "none";
+    second.checked = false;
+    first.checked = true;
 }
 
 function company () {
@@ -17,7 +22,7 @@ function company () {
     let field3 = document.getElementById("field3");
     let check = document.getElementById("check");
     let btn1 = document.getElementById("btn1");
-
+    let first = document.getElementById("first");
 
     field2.style.display = "inline";
     field3.style.display = "inline";
@@ -25,4 +30,6 @@ function company () {
     field1.style.display = "none";
     check.style.display = "none";
     btn1.style.display = "none";
+    first.checked = false;
+    second.checked = true;
 }
